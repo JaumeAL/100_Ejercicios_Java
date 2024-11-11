4316146688f2196bfc9bfd7df59748c24fc453f1
JaumeAI <jsalas2@nigul.cide.es>
2024-11-11 12:27:10 +0100

    Pide 10 numeros y da los positivos negativos y ceros

---
+++ proyecto_9/src/main/java/com/example/Main.java
@@ -0,0 +1,29 @@
+// Programa pide 10 números enteros
+//Dará el numero de positivos, de negativos y de ceros.
+
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero = 0, cantidadPositivos = 0, cantidadNegativos = 0, cantidadCeros = 0;
+
+        System.out.println("---Introduce 10 números---");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Numero :");
+            numero = sc.nextInt();
+            if (numero > 0) {
+                cantidadPositivos++;
+            } else if (numero < 0) {
+                cantidadNegativos++;
+            } else {
+                cantidadCeros++;
+            }
+        }
+        System.out.println("Número de positivos = "+ cantidadPositivos);
+        System.out.println("Número de negativos = "+ cantidadNegativos);
+        System.out.println("Número de ceros = "+ cantidadCeros);
+    }
+}
